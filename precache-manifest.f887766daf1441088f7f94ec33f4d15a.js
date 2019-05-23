68d609d4107c92dc9ce117a34ee628c7781bef36
rezan21 <reza.naghshineh@gmail.com>
2019-05-23 22:37:44 +0100

    Updates

---
+++ precache-manifest.f887766daf1441088f7f94ec33f4d15a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c9fc1e2e2acee73fc1a7129acdcb3e",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "d7df286706c6c6ab1324",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f76ef4a9451959709f7a",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d7df286706c6c6ab1324",
+    "url": "/contactmanager/static/js/2.baf3c1c8.chunk.js"
+  },
+  {
+    "revision": "f76ef4a9451959709f7a",
+    "url": "/contactmanager/static/js/main.81110979.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
